e29ef86af1558e80ffd1dd89a1e483e9e62ef93e
ev-grebenyuk <68594418+ev-grebenyuk@users.noreply.github.com>
2023-06-25 13:57:35 +0300

    Add files via upload

--- mkdir_by_date.bat
+++ mkdir_by_date.bat
@@ -1,8 +1,11 @@
+@echo off
 rem Программа по созданию директории с именем в формате текущей даты
 
 rem В переменной directory указать путь к директории, в которой следует создавать папки по датам
 set directory=E:\Test\
 
 rem Основная программа
-set directory_name=%DATE:~-4%_%DATE:~3,2%_%DATE:~0,2%
-mkdir %directory%%directory_name%
+if not exist %directory%%directory_name% (
+  set directory_name=%DATE:~-4%_%DATE:~3,2%_%DATE:~0,2%
+  mkdir %directory%%directory_name%
+)
